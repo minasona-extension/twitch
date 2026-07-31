9201b9e43e03e5ef7968663425dd47a7f6e43120
HellPing <maddin.maddin@yahoo.com>
2026-07-31 23:09:50 +0200

    Fixed: Palsonas from inactive communities no longer appear.

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -44,6 +44,7 @@ async function updateMinasonaMap() {
 
     const reducedData: MinasonaStorage = {};
     Object.entries(data).forEach(([communityName, members]) => {
+      if (!communityData.channels[communityName]) return;
       members.forEach((m) => {
         if (!m.twitchUsername) return;
         const lowerCaseUsername = m.twitchUsername.toLowerCase();
